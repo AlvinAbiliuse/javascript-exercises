aedbccb98bd8e2c365e88e8ca25d886313026a82
AlvinAbiluse <alvinabiliuse@gmail.com>
2024-12-11 03:19:38 +0400

    completed 02_repeatString js exercise from TOP foundations

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(str, n) {
+	let message = "";
+	if (n < 0) return "ERROR";
+	for (i=0; i < n; i++) message+= str;
+	return message
 };
+
 
 // Do not edit below this line
 module.exports = repeatString;
