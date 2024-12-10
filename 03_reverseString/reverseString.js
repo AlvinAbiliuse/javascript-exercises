839fe16608da22cf2d743fc0aebf26504bd1a31d
AlvinAbiluse <alvinabiliuse@gmail.com>
2024-12-11 03:33:28 +0400

    completed 03_reversString js Exercise from TOP js foundations

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,14 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+	let messageList = new Array();
+	for (i in string){
+		messageList.push(string[i]);
+	}
+	let message = "";
+	l = messageList.length - 1;
+	for (i=l; i >= 0; i--) {
+		message+= messageList[i];
+	}
+	return message;
 };
 
 // Do not edit below this line
