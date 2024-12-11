1b6d98d6c25fe5846169dd42c133e7ec9f74137f
AlvinAbiluse <alvinabiliuse@gmail.com>
2024-12-12 01:49:32 +0400

    completed 11_getTheTitles js exercise from TOP js exercises

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,20 @@
-const getTheTitles = function() {
+const books = [
+  {
+    title: 'Book',
+    author: 'Name'
+  },
+  {
+    title: 'Book2',
+    author: 'Name2'
+  }
+]
 
+const getTheTitles = function(books) {
+	let tempArray = new Array();
+	for (i in books) {
+		tempArray.push(books[i].title);
+	}
+	return tempArray;
 };
 
 // Do not edit below this line
