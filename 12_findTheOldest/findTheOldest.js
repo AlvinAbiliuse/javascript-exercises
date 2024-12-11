ac6d2cf1592c19225e594af91e79122d1b4fae38
AlvinAbiluse <alvinabiliuse@gmail.com>
2024-12-12 02:26:59 +0400

    completed 12_findTheOldest js exercise from TOP js foundations

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,17 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+	let oldest;
+	let age = 0;
+	for (i in people) {
+		if (people[i].yearOfDeath == undefined) {
+			let year = new Date()
+			people[i].yearOfDeath = year.getFullYear();
+		}
+		if ((people[i].yearOfDeath - people[i].yearOfBirth) > age){
+			oldest = people[i]
+			age = people[i].yearOfDeath - people[i].yearOfBirth
+		}
+	}
+	return oldest
 };
 
 // Do not edit below this line
