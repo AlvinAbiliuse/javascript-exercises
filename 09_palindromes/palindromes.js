58462320f52a01572a94bc93cff5cbfe0f9c8dea
AlvinAbiluse <alvinabiliuse@gmail.com>
2024-12-12 01:08:07 +0400

    completed 09_palindromes js exercise from TOP js foundations

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,22 @@
-const palindromes = function () {
+const palindromes = function (word) {
+	let wordString = "";
+	for (i in word) {
+		if (["!", ",", " ", "."].includes(word[i])) {
+			continue;
+		} else {
+			wordString += word[i].toLowerCase()
+		}
+	}
+	let reverseString = "";
+	for (i = wordString.length - 1; i >= 0; i--) {
+		console.log(i);
+		reverseString += wordString[i].toLowerCase()
+	}
+	if (wordString == reverseString) {
+		return true;
+	} else {
+		return false;
+	}
 
 };
 
