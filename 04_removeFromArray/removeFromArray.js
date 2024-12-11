56cd3ac384e8538fe119e7c57449b090c846097d
AlvinAbiluse <alvinabiliuse@gmail.com>
2024-12-11 19:58:34 +0400

    completed 04_removeFromArray js exercise from TOP js foundations

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,18 @@
-const removeFromArray = function() {
-};
+const removeFromArray = function(...args) {
+	let array = args[0];
+	let toRemove = args;
+	toRemove.shift();
+	let len = array.length;
+	for (i = 0; i < len; i++) {
+		for (j=0; j < toRemove.length; j++) {
+			if (array[i] === toRemove[j]) {
+				array.splice(i, 1);
+				i--; len--;
+			}
+		}
+	}
+	return array;
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
