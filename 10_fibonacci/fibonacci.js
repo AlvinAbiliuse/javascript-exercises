444059aed40706df27c05dbabae468c3fc2ec670
AlvinAbiluse <alvinabiliuse@gmail.com>
2024-12-12 01:39:52 +0400

    completed 10_fibonacci js exercise from TOP js foundations

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+	let nList = [0, 1]
+	if (Number(n) >= 0) {
+		for (i=2; i <= Number(n); i++) {
+			nList.push(nList[i-1] + nList[i-2])
+		}
+	} else {
+		return "OOPS"
+	}
+	return nList[Number(n)];
 };
 
 // Do not edit below this line
