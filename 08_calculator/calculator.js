d3f727171493f46de2295aeb6e42ec19ea6461bf
AlvinAbiluse <alvinabiliuse@gmail.com>
2024-12-12 00:50:13 +0400

    completed 08_calculator js exercise from TOP js foundations

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,37 @@
-const add = function() {
-	
+const add = function(n1, n2) {
+	return n1 + n2;	
 };
 
-const subtract = function() {
-	
+const subtract = function(n1, n2) {
+	return n1 - n2	
 };
 
-const sum = function() {
-	
+const sum = function(nList) {
+	let sum = 0;
+	for (i in nList) {
+		sum+= nList[i];
+	}
+	return sum;	
 };
 
-const multiply = function() {
-
+const multiply = function(nList) {
+	let multiply = nList[0];
+	for (i=1; i < nList.length; i++) {
+		multiply = multiply * nList[i];
+	}
+	return multiply;
 };
 
-const power = function() {
-	
+const power = function(n1, n2) {
+	return n1 ** n2;	
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+	let factorial = 1;
+	for (i=1; i <= n; i++) {
+		factorial = factorial * i;
+	}
+	return factorial;	
 };
 
 // Do not edit below this line
