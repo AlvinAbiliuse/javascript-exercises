e3dfcee3b31171c0a456b5ef18c1dc4c510f6e03
AlvinAbiluse <alvinabiliuse@gmail.com>
2024-12-12 00:13:45 +0400

    completed 05_sumAll js exercise from TOP js foundations

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
-
+const sumAll = function(n1, n2) {
+	let number = 0
+	for (i in arguments) {
+		if (typeof arguments[i] != "number" || arguments[i] < 0 ||
+			arguments[i] % 1 != 0) {
+			return "ERROR"
+		}
+	}
+	if (n1 < n2) {
+		 for (i = n1; i <= n2; i++) {
+			 number+= i;
+		}
+	} else if (n2 < n1) {
+		 for (i = n2; i <= n1; i++){
+			 number+= i;
+		}
+	}
+	return number;	
 };
 
 // Do not edit below this line
